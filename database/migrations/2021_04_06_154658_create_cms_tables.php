b10bf762b3a5123c6724d8f0f6fff07c2e0aed17
xtrees <649223152@qq.com>
2021-05-26 00:13:15 +0800

    init

--- database/migrations/2021_04_06_154658_create_cms_tables.php
+++ database/migrations/2021_04_06_154658_create_cms_tables.php
@@ -13,6 +13,41 @@ class CreateCmsTables extends Migration
      */
     public function up()
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('role_id')->default(1);
+            $table->string('name', 50)->unique();
+            $table->string('email', 100)->unique();
+            $table->string('phone', 11)->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('password', 200);
+            $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->unsignedInteger('coins')->default(0)->comment('金币数量');
+            $table->boolean('blocked')->default(0)->comment('锁定');
+            $table->string('source', 20)->default('web')->comment('注册来源');
+            $table->timestamp('activated_at')->nullable()->comment('活跃时间');
+            $table->boolean('robot')->default(false);
+            $table->timestamp('role_expired_at')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('icon')->nullable();
+            $table->unsignedInteger('permission')->default(0)->comment('权限等级');
+            $table->timestamps();
+        });
+
+
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->morphs('favoriteable');
+            $table->timestamps();
+        });
+
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
@@ -22,7 +57,7 @@ class CreateCmsTables extends Migration
             $table->string('summary', 200)->default('');
             $table->unsignedInteger('total')->default(0)->comment('总计文章');
             $table->unsignedInteger('sort')->default(0);
-            $table->unsignedInteger('level')->default(0)->comment('权限等级');
+            $table->unsignedInteger('permission')->default(0)->comment('权限等级');
             $table->boolean('display')->default(false);
             $table->timestamps();
         });
@@ -38,17 +73,22 @@ class CreateCmsTables extends Migration
             $table->text('body')->nullable();
             $table->unsignedInteger('views')->default(0);
             $table->unsignedInteger('favorites')->default(0);
-            $table->unsignedInteger('level')->default(0)->comment('权限等级');
+            $table->unsignedInteger('permission')->default(0)->comment('权限等级');
+            $table->unsignedInteger('coins')->default(0)->comment('金币');
+            $table->timestamp('published_at')->nullable();
+            $table->boolean('display')->default(true);
             $table->timestamps();
         });
 
 
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('content_id')->nullable();
+            $table->morphs('imageable');
             $table->string('storage', 20)->default('local');
             $table->string('path');
             $table->boolean('cover')->default(false);
+            $table->unsignedSmallInteger('sort')->default(0);
+            $table->boolean('display')->default(true);
             $table->timestamps();
         });
 
@@ -61,6 +101,7 @@ class CreateCmsTables extends Migration
             $table->string('keywords', 100)->default('');
             $table->string('summary', 200)->default('');
             $table->unsignedInteger('total')->default(0)->comment('总数');
+            $table->boolean('display')->default(true);
             $table->integer('sort')->default(0);
             $table->timestamps();
         });
@@ -72,13 +113,13 @@ class CreateCmsTables extends Migration
             $table->string('keywords', 100)->default('');
             $table->string('summary', 200)->default('');
             $table->integer('sort')->default(0);
+            $table->boolean('display')->default(false);
             $table->timestamps();
         });
 
-        Schema::create('taggable', function (Blueprint $table) {
+        Schema::create('taggables', function (Blueprint $table) {
             $table->foreignId('tag_id');
-            $table->foreignId('content_id');
-            $table->unique(['tag_id', 'content_id']);
+            $table->morphs('taggable');
         });
 
         Schema::create('pages', function (Blueprint $table) {
@@ -90,15 +131,27 @@ class CreateCmsTables extends Migration
             $table->string('summary', 200)->default('');
             $table->string('template')->default('default');
             $table->json('data');
-            $table->unsignedInteger('level')->default(0)->comment('权限等级');
+            $table->unsignedInteger('permission')->default(0)->comment('权限等级');
+            $table->boolean('display')->default(false);
+            $table->timestamps();
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->text('content');
+            $table->unsignedInteger('views')->default(0);
+            $table->unsignedInteger('favorites')->default(0);
+            $table->unsignedInteger('coins')->default(0)->comment('金币');
+            $table->boolean('display')->default(true);
             $table->timestamps();
         });
 
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('content_id');
-            $table->text('content')->nullable();
+            $table->morphs('commentable');
+            $table->string('content')->nullable();
             $table->unsignedInteger('likes')->default(0)->comment('点赞');
             $table->boolean('display')->default(true);
             $table->timestamps();
@@ -113,11 +166,11 @@ class CreateCmsTables extends Migration
             $table->string('summary', 200)->default('');
             $table->text('body')->nullable();
             $table->boolean('display')->default(true);
-            $table->unsignedInteger('level')->default(0)->comment('权限等级');
+            $table->unsignedInteger('permission')->default(0)->comment('权限等级');
             $table->timestamps();
         });
 
-        Schema::create('content_collection', function (Blueprint $table) {
+        Schema::create('collection_contents', function (Blueprint $table) {
             $table->unsignedBigInteger('content_id');
             $table->unsignedBigInteger('collection_id');
             $table->unsignedInteger('sort')->default(0);
@@ -133,6 +186,9 @@ class CreateCmsTables extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('user_role');
         Schema::dropIfExists('categories');
         Schema::dropIfExists('contents');
         Schema::dropIfExists('images');
